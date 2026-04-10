829f9e03c649b6908cff01cd7b4b9ad8c0532ffa
salvic928-spec <salvic928@gmail.com>
2026-04-10 19:55:12 +0100

    Update paneleiro.js

--- paneleiro.js
+++ paneleiro.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 
 const app = express();
 const PORT = 3000;
-
+const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
 app.use(cors());
 app.use(express.json());
 app.use(express.static(__dirname));
